8737932ae2db6371d95703e076c6b635cf376f1b
Justin Ehlert <justinehlert@flywheel.io>
2018-07-06 11:13:20 -0500

    Add concurrent signed-url upload support

--- flywheel_cli/config.py
+++ flywheel_cli/config.py
@@ -14,6 +14,8 @@ class Config(object):
         if self.cpu_count == -1:
             self.cpu_count = max(1, math.floor(multiprocessing.cpu_count() / 2))
 
+        self.concurrent_uploads = getattr(args, 'concurrent_uploads', 4)
+
         self.follow_symlinks = getattr(args, 'symlinks', False)
 
         self.buffer_size = 65536
@@ -26,6 +28,7 @@ class Config(object):
     @staticmethod
     def add_config_args(parser):
         parser.add_argument('--jobs', '-j', default=-1, type=int, help='The number of concurrent jobs to run (e.g. compression jobs)')
+        parser.add_argument('--concurrent-uploads', default=4, type=int, help='The maximum number of concurrent uploads')
         parser.add_argument('--compression-level', default=1, type=int, choices=range(-1, 9), 
                 help='The compression level to use for packfiles. -1 for default, 0 for store')
         parser.add_argument('--symlinks', action='store_true', help='follow symbolic links that resolve to directories')
--- flywheel_cli/sdk_impl.py
+++ flywheel_cli/sdk_impl.py
@@ -3,12 +3,27 @@ import copy
 import flywheel
 import json
 import os
+import requests
 import sys
 
 from .importers import Uploader, ContainerResolver
 
 CONFIG_PATH = '~/.config/flywheel/user.json'
 config = None
+
+TICKETED_UPLOAD_PATH = '/{ContainerType}/{ContainerId}/files'
+
+def pluralize(container_type):
+    """ Convert container_type to plural name
+    
+    Simplistic logic that supports:
+    group,  project,  session, subject, acquisition, analysis, collection
+    """
+    if container_type == 'analysis':
+        return 'analyses'
+    if not container_type.endswith('s'):
+        return container_type + 's'
+    return container_type
 
 def load_config():
     global config
@@ -40,6 +55,15 @@ class SdkUploadWrapper(Uploader, ContainerResolver):
 
     def __init__(self, fw):
         self.fw = fw
+        self._supports_signed_url = None
+        # Session for signed-url uploads
+        self._upload_session = requests.Session()
+
+    def supports_signed_url(self):
+        if self._supports_signed_url is None:
+            config = self.fw.get_config()
+            self._supports_signed_url = config.get('signed_url', False)
+        return self._supports_signed_url
 
     def resolve_path(self, container_type, path):
         parts = path.split('/')
@@ -90,13 +114,58 @@ class SdkUploadWrapper(Uploader, ContainerResolver):
         return new_id
     
     def upload(self, container, name, fileobj):
-        # For now upload without progress...
         upload_fn = getattr(self.fw, 'upload_file_to_{}'.format(container.container_type), None)
 
         if not upload_fn:
             print('Skipping unsupported upload to container: {}'.format(container.container_type))
             return
 
-        upload_fn(container.id, flywheel.FileSpec(name, fileobj))
-        
+        if self.supports_signed_url():
+            self.signed_url_upload(container, name, fileobj)
+        else:
+            upload_fn(container.id, flywheel.FileSpec(name, fileobj))
+
+    def signed_url_upload(self, container, name, fileobj):
+        """Upload fileobj to container as name, using signed-urls"""
+        # Create ticketed upload
+        path_params = {
+            'ContainerType': pluralize(container.container_type),
+            'ContainerId': container.id
+        }
+        ticket, upload_url = self.create_upload_ticket(path_params, name)
+
+        # Perform the upload
+        resp = self._upload_session.put(upload_url, data=fileobj)
+        resp.raise_for_status()
+        resp.close()
+
+        # Complete the upload
+        self.complete_upload_ticket(path_params, ticket)
+
+    def create_upload_ticket(self, path_params, name):
+        body = {
+            'metadata': {},
+            'filenames': [ name ]
+        }
+
+        response = self.call_api(TICKETED_UPLOAD_PATH, 'POST',
+            path_params=path_params, 
+            query_params=[('ticket', '')],
+            body=body,
+            response_type=object
+        )
+
+        return response['ticket'], response['urls'][name]
+
+    def complete_upload_ticket(self, path_params, ticket):
+        self.call_api(TICKETED_UPLOAD_PATH, 'POST',
+            path_params=path_params,
+            query_params=[('ticket', ticket)])
+
+    def call_api(self, resource_path, method, **kwargs):
+        kwargs.setdefault('auth_settings', ['ApiKey'])
+        kwargs.setdefault('_return_http_data_only', True)
+        kwargs.setdefault('_preload_content', True)
+
+        return self.fw.api_client.call_api(resource_path, method, **kwargs)
 
