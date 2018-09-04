bc59771977b10dc6404cf7a5a26578281923d58e
Justin Ehlert <justinehlert@flywheel.io>
2018-09-04 10:26:23 -0500

    Update requirements

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ VERSION = "6.1.0"
 REQUIRES = [
     "tzlocal~=1.5.1",
     "fs~=2.0.23",
-    "flywheel_migration==0.2.0",
-    "flywheel-bids~=0.6.1"
+    "flywheel_migration==0.3.0",
+    "flywheel-bids~=0.6.8"
 ]
 
 class VerifyVersionCommand(install):
