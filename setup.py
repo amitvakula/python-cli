1563e3efebf66061453362eec0fbff6a0cde843e
Justin Ehlert <justinehlert@flywheel.io>
2018-12-21 14:48:50 -0600

    Update CLI version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 NAME = "flywheel-cli"
-VERSION = "7.0.3"
+VERSION = "7.1.0"
 
 # To install the library, run the following
 #
