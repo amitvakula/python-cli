6d5765b9c24d06ee16cde948c81ac48355748456
mjburling <michaelburling@flywheel.io>
2019-02-04 13:35:45 -0600

    Up version to 7.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 NAME = "flywheel-cli"
-VERSION = "7.1.1"
+VERSION = "7.1.2"
 
 # To install the library, run the following
 #
