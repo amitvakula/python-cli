35134e21361a88fb96f37e9f64a5d425cb3e77e0
Justin Ehlert <justinehlert@flywheel.io>
2019-05-28 10:00:38 -0500

    Refactor setup (#2)
    * Move requirements into setup.py
    * Add pylint step
    * Add README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Flywheel CLI
+
+**flywheel-cli** is a library and command line interface for interacting with a Flywheel site.
+
+## Building
+
+It's recommended that you use [pipenv](https://docs.pipenv.org/en/latest/) to manage dependencies. For example:
+```
+> python3 -m pip install pipenv
+> pipenv install -e .[dev]
+```
+
+## Testing
+
+Tests and python linting can be done using the standard tools:
+```
+> pipenv run pytest tests/unit_tests
+> pipenv run pylint --rcfile .pylintrc flywheel_cli/
+```
--- setup.py
+++ setup.py
@@ -18,8 +18,23 @@ NAME = "flywheel-cli"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+INSTALL_REQUIRES = [
+    'boto3>=1.9.0',
+    'crayons>=0.2.0',
+    'fs>=2.4.4',
+    'flywheel-bids~=0.8.0',
+    'flywheel_migration>=0.10.0',
+    'flywheel-sdk>=8.0.0',
+    'pyinstaller~=3.3.1',
+    'tzlocal~=1.5.1',
+]
+
+DEV_REQUIRES = [
+    'pycodestyle~=2.4.0',
+    'pylint~=2.3.0',
+    'pytest~=3.6.0',
+    'pytest-cov~=2.5.0',
+]
 
 with open('flywheel_cli/VERSION') as f:
     version = f.read().strip()
@@ -32,7 +47,10 @@ setup(
     url="",
     keywords=["Flywheel", "flywheel", "CLI"],
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        'dev': DEV_REQUIRES
+    },
     packages=find_packages(),
     license="MIT",
     project_urls={
